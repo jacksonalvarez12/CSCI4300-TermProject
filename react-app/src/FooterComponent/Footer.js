21f4106a034e2229d8a4b4202b0a89daf191905f
colbyEskew <83030360+colbyEskew@users.noreply.github.com>
2022-11-09 16:16:33 -0500

    Update Footer.js

--- react-app/src/FooterComponent/Footer.js
+++ react-app/src/FooterComponent/Footer.js
@@ -1,28 +1,11 @@
 import './Footer.css';
 import React from 'react';
 
-/*const Box = styled.div`
-  padding: 80px 60px;
-  background: black;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-`;
-
 const Footer = () => {
-    return (
-        <Box>
-            <button>Add Recipie</button>
-            <p>Created by Colby Eskew, Jackson Alvarez, and Joshua Bisnott</p>
-        </Box>
-    );
-}*/
-
-
-// I added this version of footer until colby fixes it so it loads
-const Footer = () => {
-    return (
-        <p>This is the Footer</p>
-    );
+    return <footer>
+        <div className='add-button' >Add</div>
+        <p>Created by Colby Eskew, Jackson Alvarez, and Joshua Bisnott</p>
+        </footer>
+    
 }
 export default Footer;
