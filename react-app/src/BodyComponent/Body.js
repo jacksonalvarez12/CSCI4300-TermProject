d275cfbb4a2604944a09fea030ae3b674ff83acd
jacksonalvarez12 <jacksonalvarezacc@gmail.com>
2022-11-10 11:45:41 -0500

    Fixed css

--- react-app/src/BodyComponent/Body.js
+++ react-app/src/BodyComponent/Body.js
@@ -10,9 +10,25 @@ class Body extends React.Component {
     // auth not working yet
   }
 
+  // <p> elements just to test css
   render() {
     return (
-      <p>This is the body</p>
+      <div className="body">
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      <p style={{'font-size':'24px'}}>This is the body</p>
+      </div>
     );
   }
 }
