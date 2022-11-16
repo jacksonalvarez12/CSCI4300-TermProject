550f62c8c065040c80eb045477463f1c48204418
jacksonalvarez12 <jacksonalvarezacc@gmail.com>
2022-11-16 12:34:40 -0500

    Small css change

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -13,6 +13,7 @@ class App extends React.Component {
 
   render() {
     return <>
+    <p>{this.state.auth}</p>
         <Header auth={this.state.auth} gettingLogin={false} gettingSignup={false} sendAuth={this.setAuth}/>
         <Body auth={this.state.auth}/>
         <Footer auth={this.state.auth} adding={false} sendAuth={this.setAuth}/>
