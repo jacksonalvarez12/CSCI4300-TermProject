7b2501494a178eaace1c8ef89844d84c450133c0
jacksonalvarez12 <jacksonalvarezacc@gmail.com>
2022-11-17 15:48:51 -0500

    Implemented view and made css changes

--- react-app/src/CardComponent/Card.js
+++ react-app/src/CardComponent/Card.js
@@ -13,7 +13,6 @@ class Card extends React.Component {
       ingredients: this.props.ingredients,
       link: this.props.link
     }
-    // auth not working yet
   }
 
   render() {
@@ -36,17 +35,49 @@ class Card extends React.Component {
         />
         <ul>{ingsHTML}</ul>
         <a href={this.state.link}>Link to Recipe!</a>
-        <div className='card-buttons'>
-          <button onClick={() => this.view()}>View</button>
-          <button onClick={() => this.edit()}>Edit</button>
-          <button onClick={() => this.delete()}>Delete</button>
+        <div className='card-buttons' id='card-buttons-container'>
+          <button id={'view-card' + this.state.id} onClick={() => this.view()}>View</button>
+          <button id={'edit-card' + this.state.id} onClick={() => this.edit()}>Edit</button>
+          <button id={'delete-card' + this.state.id} onClick={() => this.delete()}>Delete</button>
+          <button id={'exit-view-card' + this.state.id} style={{display: 'none'}} onClick={() => this.exitView()}>Exit View</button>
         </div>
         </div>
     );
   }
 
   view() {
-    alert('view not implemented');
+    let card = document.getElementById('card' + this.state.id);
+    card.style.backgroundColor = 'pink';
+    card.style.maxWidth = "none";
+    card.style.width = '80%';
+    
+    let box = document.getElementById('card-container');
+    let allCards = Array.from(box.childNodes)
+    let otherCards = allCards.filter(card => card.id !== 'card' + this.state.id);
+    otherCards.forEach(card => card.style.display = 'none');
+
+    document.getElementById('view-card' + this.state.id).style.display = 'none';
+    document.getElementById('edit-card' + this.state.id).style.display = 'none';
+    document.getElementById('delete-card' + this.state.id).style.display = 'none';
+    document.getElementById('exit-view-card' + this.state.id).style.display = 'inline';
+
+  }
+
+  exitView() {
+    let card = document.getElementById('card' + this.state.id);
+    card.style.backgroundColor = 'rgb(237, 218, 193)';
+    card.style.maxWidth = "400px";
+    card.style.width = 'auto';
+
+    document.getElementById('view-card' + this.state.id).style.display = 'inline';
+    document.getElementById('edit-card' + this.state.id).style.display = 'inline';
+    document.getElementById('delete-card' + this.state.id).style.display = 'inline';
+    document.getElementById('exit-view-card' + this.state.id).style.display = 'none';
+
+    let box = document.getElementById('card-container');
+    let allCards = Array.from(box.childNodes)
+    let otherCards = allCards.filter(card => card.id !== 'card' + this.state.id);
+    otherCards.forEach(card => card.style.display = 'flex');
   }
 
   edit() {
